a1013fa8c071d197d07e064589d4b22c83d2cdbd
LIMoreno04 <ignamoreno04@gmail.com>
2026-05-25 23:21:24 -0300

    feat: added navigation between login and signup pages

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -7,11 +7,19 @@ import Input from "@/components/Input";
 
 export default function LoginForm() {
   return (
-    <form action={login} className="max-w-md mx-auto mt-10">
-      <Card title="Log in" >
-        <Input name="email" variant="text" placeholder="Email" />
-        <Input name="password" variant="password" placeholder="Password" />
-        <Button type="submit">Log in</Button>
+    <form action={login} className="mx-auto mt-10">
+      <Card title="Log in" className="w-2xl" >
+        <div className="flex flex-col items-center gap-4 w-full">
+          <div className="flex flex-col items-center gap-4 w-full mb-8">
+            <Input name="email" variant="text" placeholder="Email" className="w-3/4" />
+            <Input name="password" variant="password" placeholder="Password" className="w-3/4" />
+          </div>
+          <Button type="submit" width="75%">Log in</Button>
+          <div className="flex flex-col items-center mt-4">
+            <span className="w-full text-center">Do not have an account yet?</span>
+            <Button type="button" variant="text" className="w-fit" onClick={() => window.location.href = "/signup"}>Sign up</Button>
+          </div>
+        </div>
       </Card>
     </form>
   );
