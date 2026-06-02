0f83787c021535639110bdae7214ed78a9ace38b
LIMoreno04 <ignamoreno04@gmail.com>
2026-06-02 20:23:01 -0300

    feat: login page final version

---
+++ frontend/app/login/loginForm.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { login } from "@/lib/api/auth";
+import Button from "@/components/Button";
+import Card from "@/components/Card";
+import Input from "@/components/Input";
+
+export default function LoginForm() {
+  return (
+    <div>
+      <form action={login} className="mx-auto mt-10">
+        <Card title="Log in" className="w-2xl" >
+          <div className="flex flex-col items-center gap-4 w-full">
+            <div className="flex flex-col items-center gap-4 w-full mb-8">
+              <Input name="email" variant="text" placeholder="Email" className="w-3/4" />
+              <Input name="password" variant="password" placeholder="Password" className="w-3/4" />
+            </div>
+            <Button type="submit" width="75%">Log in</Button>
+            <div className="flex flex-col items-center mt-4">
+              <span className="w-full text-center">Do not have an account yet?</span>
+              <Button type="button" variant="text" className="w-fit" onClick={() => window.location.href = "/signup"}>Sign up</Button>
+            </div>
+          </div>
+        </Card>
+      </form>
+    </div>
+  );
+}
--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -1,26 +1,19 @@
-"use client";
+import LoginForm from "./loginForm";
+import Image from "next/image";
 
-import { login } from "@/lib/api/auth";
-import Button from "@/components/Button";
-import Card from "@/components/Card";
-import Input from "@/components/Input";
-
-export default function LoginForm() {
+export default function LoginPage() {
   return (
-    <form action={login} className="mx-auto mt-10">
-      <Card title="Log in" className="w-2xl" >
-        <div className="flex flex-col items-center gap-4 w-full">
-          <div className="flex flex-col items-center gap-4 w-full mb-8">
-            <Input name="email" variant="text" placeholder="Email" className="w-3/4" />
-            <Input name="password" variant="password" placeholder="Password" className="w-3/4" />
-          </div>
-          <Button type="submit" width="75%">Log in</Button>
-          <div className="flex flex-col items-center mt-4">
-            <span className="w-full text-center">Do not have an account yet?</span>
-            <Button type="button" variant="text" className="w-fit" onClick={() => window.location.href = "/signup"}>Sign up</Button>
-          </div>
+    <div className="flex h-full items-center justify-center">
+      <div className="hidden md:flex w-1/3 mr-16 flex-col justify-between items-baseline h-full pt-16">
+        <Image src="/isotipo.svg" alt="Lakitu" width={1000} height={1000} />
+        <div className="relative w-full h-1/8">
+          <Image src="/sombra.svg" alt="Sombra" width={320} height={10} style={{position:"absolute", top:"0", left:"0", transform:"translateX(-20px)" }}/>
+          <Image src="/sombra.svg" alt="Sombra" width={180} height={50} style={{position:"absolute", bottom:"0", right:"0", transform:"translateX(-20px)" }}/>        
         </div>
-      </Card>
-    </form>
+      </div>
+      <div>
+        <LoginForm />
+      </div>
+    </div>
   );
 }
