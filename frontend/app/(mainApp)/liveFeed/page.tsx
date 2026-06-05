63fae4f2584adb681e2d8a1c51a13dfc80966f33
LIMoreno04 <ignamoreno04@gmail.com>
2026-06-05 15:07:48 -0300

    fix: websocket bug when using the app router

--- frontend/app/(mainApp)/liveFeed/page.tsx
+++ frontend/app/(mainApp)/liveFeed/page.tsx
@@ -1,18 +1,12 @@
 "use client";
 
-import { useRouter } from "next/navigation";
 import { useState } from "react";
 import ParkingLotSelector from "./ParkingLotSelector";
 import Button from "@/components/Button";
 
 export default function LiveFeed() {
-  const router = useRouter();
   const [selectedLot, setSelectedLot] = useState<string | null>(null);
 
-  const handleViewFeed = () => {
-    if (!selectedLot) return;
-    router.push(`/liveFeed/${selectedLot}`);
-  };
 
   return (
     <div className="flex flex-col justify-baseline items-center w-full h-full">
@@ -21,7 +15,7 @@ export default function LiveFeed() {
         onSelect={setSelectedLot}
       />
 
-      <Button className="mt-8" disabled={!selectedLot} onClick={handleViewFeed}>
+      <Button className="mt-8" disabled={!selectedLot} onClick={() => (window.location.href = `/liveFeed/${selectedLot}`)}>
         View Live Feed
       </Button>
     </div>
