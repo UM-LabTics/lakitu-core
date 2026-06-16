e892c15abee1b33dd17d7a6b5cd8e53c14df8161
LIMoreno04 <ignamoreno04@gmail.com>
2026-06-16 17:59:12 -0300

    feat: setup for frontend deployment

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,25 +1,15 @@
 import type { NextConfig } from "next";
 
-const BUCKET = process.env.S3_BUCKET_NAME
+const BUCKET = process.env.S3_BUCKET_NAME;
 
 const nextConfig: NextConfig = {
-images: {
-  remotePatterns: [
-    {
-      protocol: "https",
-      hostname: `${BUCKET}.s3.amazonaws.com`,
-    },
-  ],
-},
-
-  webpack: (config, { dev }) => {
-    if (dev) {
-      config.watchOptions = {
-        poll: 200,
-        aggregateTimeout: 200,
-      };
-    }
-    return config;
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: `${BUCKET}.s3.amazonaws.com`,
+      },
+    ],
   },
 };
 
